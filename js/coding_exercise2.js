2b4f97770eb9560cb9ae9ae5116844735178b4aa
ue2116 <ue2116@columbia.edu>
2017-03-23 16:20:09 -0400

    Update coding_exercise2.js

--- js/coding_exercise2.js
+++ js/coding_exercise2.js
@@ -1,38 +1,30 @@
 function setup() {
-  createCanvas(5000, 5000);
+  createCanvas(500, 500);
 }
-function draw() {
-  colorMode(HSB, 100);
-  for (i = 0; i < 100; i++) {
-    for (j = 0; j < 100; j++) {
-      stroke(i, j, 100);
-      point(i, j);
+
+
+function  calcColor (dist, maxDist){
+  var ans=int((dist*1.0)/maxDist*245);
+  ans+=5;
+  return ans;
+}
+
+function calcRadius (dist, maxDist, maxRadius, minRadius){
+  var ans=int((dist*1.0)/maxDist*((maxRadius-minRadius)));
+  ans+=minRadius;
+  return ans;
+}
+
+function draw(){
+  clear();
+  for (i=0; i<20; i++){
+    for(j=0; j<20;j++){
+      var d=dist(mouseX, mouseY, i*20+10, j*20+10);
+      var col=calcColor(d,500);
+      var rad=calcRadius(d, 500, 20, 2);
+      var c= color(col, col, col);
+      fill(c);
+      ellipse(i*20+10, j*20+10, rad, rad);
+    }
   }
- }
-}
-
-function draw() {
-  background(200);
-  fill(0);
-
-  var x1 = 10;
-  var y1 = 90;
-  var x2 = mouseX;
-  var y2 = mouseY;
-
-  line(x1, y1, x2, y2);
-  ellipse(x1, y1, 7, 7);
-  ellipse(x2, y2, 7, 7);
-
-  // d is the length of the line
-  // the distance from point 1 to point 2.
-  var d = int(dist(x1, y1, x2, y2));
-
-  // Let's write d along the line we are drawing!
-  push();
-  translate( (x1+x2)/2, (y1+y2)/2 );
-  rotate( atan2(y2-y1,x2-x1) );
-  text(nfc(d,1,1), 0, -5);
-  pop();
-  // Fancy!
 }
